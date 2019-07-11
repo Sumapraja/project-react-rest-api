488f1192fedd1196033a799f480df8fd4dff3de3
Sumapraja <irfansumapraja@gmail.com>
2019-07-11 10:46:27 +0700

    added .env

--- src/App.js
+++ src/App.js
@@ -1,80 +1,153 @@
 import React from "react";
 import axios from "axios";
+import "./App.css";
 
-export default class TodosList extends React.Component {
+export default class Todos extends React.Component {
   state = {
-    todo: "",
-    sayings: []
+    description: "",
+    tasks: [],
+    showingTask: true,
+    showingEdit: false,
+    id: ""
   };
 
   handleChange = event => {
-    this.setState({
-      todo: event.target.value
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    axios
-      .post(`http://haekal-todo-list-api.herokuapp.com/todos`, {
-        todo: this.state.todo
-      })
-      .then(res => {
-        console.log(res);
-        this.setState({
-          sayings: res.data
-        });
-        console.log(res.sayings);
-      });
-  };
-
-  deleteChange = event => {
-    this.setState({ id: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   deleteSubmit = index => {
+    axios.delete(`${process.env.REACT_APP_API}/${index}`).then(res => {
+      this.setState({
+        tasks: res.data
+      });
+      if (res.status === 200) {
+        alert("Data Deleted!");
+      }
+      console.log(res);
+    });
+  };
+
+  toggleEdit = (description, id) => {
+    this.setState({
+      showingEdit: true,
+      showingTask: false,
+      id,
+      description
+    });
+  };
+
+  editTask = index => {
     axios
-      .delete(`http://haekal-todo-list-api.herokuapp.com/todos/${index}`)
+      .put(`${process.env.REACT_APP_API}/${index}`, {
+        description: this.state.description
+      })
       .then(res => {
         this.setState({
-          data: res.data
+          tasks: res.data,
+          editTask: false,
+          showingTask: true,
+          id: ""
         });
         if (res.status === 200) {
-          alert("data sudah dihapus");
+          alert("Data Updated!");
         }
       });
   };
 
   componentDidMount() {
-    axios.get(`http://haekal-todo-list-api.herokuapp.com/todos`).then(res => {
-      const data = res.data;
-      this.setState({ data });
+    axios.get(process.env.REACT_APP_API).then(res => {
+      const tasks = res.data;
+      this.setState({ tasks });
     });
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios
+      .post(process.env.REACT_APP_API, {
+        description: this.state.description
+      })
+      .then(res => {
+        console.log(res);
+        this.setState({
+          tasks: res.data
+        });
+        console.log(res.data);
+      });
+  };
+  
   render() {
     return (
-      <div>
+      <div className="App App-header">
+        <h1>Tasks</h1>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Your task today:
+            Your Task :
             <input
               type="text"
-              name="todo"
+              name="description"
               onChange={this.handleChange}
             />
           </label>
           <button type="submit">Add</button>
         </form>
-        <div>
-          {this.state.sayings.map((activity, index) => (
-            <li key={index}>
-              <div>{activity.todo}</div>
-              <div onClick={() => this.deleteSubmit(index)}>delete</div>
-            </li>
-          ))}
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+              <th>Delete</th>
+              <th>Edit/Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.tasks.map((data, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    {this.state.id === index && (
+                      <input
+                        type="text"
+                        name="description"
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                      />
+                    )}
+                    {this.state.id !== index && (
+                      <span style={{ color: "white" }}>{data.description}</span>
+                    )}
+                  </td>
+                  <td>{data.done === true ? "✓" : "x"}</td>
+                  <td>
+                    <button onClick={() => this.deleteSubmit(index)}>
+                      Delete
+                    </button>
+                  </td>
+                  <td>
+                    {this.state.showingTask === true ? (
+                      <button
+                        onClick={() => this.toggleEdit(data.description, index)}
+                      >
+                        edit
+                      </button>
+                    ) : this.state.id === index ? (
+                      <button onClick={() => this.editTask(index)}>
+                        update
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => this.toggleEdit(data.description, index)}
+                      >
+                        edit
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
